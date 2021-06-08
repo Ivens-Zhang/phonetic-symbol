7223306cbacf81fbf24290fc1aea0472b335ef5e
Ivens_Zhang <ks_zhangyi@163.com>
2021-06-08 15:11:53 +0800

    update table to grid & add content

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,153 @@
 <template>
   <div class="flex justify-center mt-24">
     <div></div>
-    <div class="w-2/4">
+    <div class="w-10/12 lg:w-3/5">
       <div>
-        <p class="text-3xl mb-5 text-center">国际音标表</p>
+        <p class="text-3xl mb-5 text-center">国x际音标表</p>
 
         <hr />
 
         <p class="text-xl mb-5 p-3">
-          48个国际音标通常是国内学生学习英语、学好英语发音必须掌握的发音基础，48个国际音标表也被称作48个音标表、48个英语音标表、48个英语国际英标表，48个国际英语音标表，这些称呼通常都是指48个英语国际英标表。48个国际音标中有20个元音、28个辅音，为了便于同学们学习和记忆巴士英语网为大家整理了这些音标表，希望能帮助到大家!
+          48个国际音标中有20个元音、28个辅音，为了便于同学们学习和记忆巴士英语网为大家整理了这些音标表，希望能帮助到大家!
         </p>
       </div>
-      <table
-        class="
-          mt-5
-          table-auto
-          border-separate border border-green-800
-          font-serif
-        "
-      >
-        <thead>
-          <tr>
-            <th class="border border-green-600">Title</th>
-            <th class="border border-green-600">Author</th>
-            <th class="border border-green-600">Views</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="border border-green-600" @mousemove="playAudio('01')">
-              Chris
-            </td>
-            <td class="border border-green-600" @mousemove="playAudio('02')">
-              Chris
-            </td>
-            <td class="border border-green-600" @mousemove="playAudio('03')">
-              Chris
-            </td>
-            <td class="border border-green-600" @mousemove="playAudio('04')">
-              Chris
-            </td>
-            <td class="border border-green-600" @mousemove="playAudio('05')">
-              Chris
-            </td>
-            <td class="border border-green-600" @mousemove="playAudio('06')">
-              Chris
-            </td>
-            <td class="border border-green-600" @mousemove="playAudio('07')">
-              Chris
-            </td>
-          </tr>
-        </tbody>
-      </table>
+
+      <div class="grid grid-cols-8 gap-2 w-full text-center my-5">
+        <div class="bg-gray-400 flex justify-center items-center row-span-5">
+          元音
+        </div>
+        <div class="bg-gray-400 flex justify-center items-center row-span-3">
+          单元音
+        </div>
+        <div class="bg-gray-400">前元音</div>
+        <div class="bg-gray-400" @click="playAudio('iː')">/iː/</div>
+        <div class="bg-gray-400" @click="playAudio('ɪ')">/ɪ/</div>
+        <div class="bg-gray-400" @click="playAudio('e')">/e/</div>
+        <div class="bg-gray-400" @click="playAudio('æ')">/æ/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400">中元音</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400" @click="playAudio('ɜː')">/ɜː/</div>
+        <div class="bg-gray-400" @click="playAudio('ə')">/ə/</div>
+        <div class="bg-gray-400" @click="playAudio('ʌ')">/ʌ/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400">后元音</div>
+        <div class="bg-gray-400" @click="playAudio('uː')">/uː/</div>
+        <div class="bg-gray-400" @click="playAudio('ʊ')">/ʊ/</div>
+        <div class="bg-gray-400" @click="playAudio('ɔː')">/ɔː/</div>
+        <div class="bg-gray-400" @click="playAudio('ɒ')">/ɒ/</div>
+        <div class="bg-gray-400" @click="playAudio('ɑː')">/ɑː/</div>
+        <div class="bg-gray-400 flex justify-center items-center row-span-2">
+          双元音
+        </div>
+        <div class="bg-gray-400">开合双元音</div>
+        <div class="bg-gray-400" @click="playAudio('eɪ')">/eɪ/</div>
+        <div class="bg-gray-400" @click="playAudio('aɪ')">/aɪ/</div>
+        <div class="bg-gray-400" @click="playAudio('ɔɪ')">/ɔɪ/</div>
+        <div class="bg-gray-400" @click="playAudio('aʊ')">/aʊ/</div>
+        <div class="bg-gray-400" @click="playAudio('əʊ')">/əʊ/</div>
+        <div class="bg-gray-400">集中双元音</div>
+        <div class="bg-gray-400" @click="playAudio('ɪə')">/ɪə/</div>
+        <div class="bg-gray-400" @click="playAudio('eə')">/eə/</div>
+        <div class="bg-gray-400" @click="playAudio('ʊə')">/ʊə/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div
+          class="bg-gray-400 flex justify-center items-center"
+          style="grid-row: span 9 / span 9"
+        >
+          辅音
+        </div>
+        <div class="bg-gray-400 flex justify-center items-center row-span-2">
+          爆破音
+        </div>
+        <div class="bg-gray-400">清辅音</div>
+        <div class="bg-gray-400" @click="playAudio('p')">/p/</div>
+        <div class="bg-gray-400" @click="playAudio('t')">/t/</div>
+        <div class="bg-gray-400" @click="playAudio('k')">/k/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400">浊辅音</div>
+        <div class="bg-gray-400" @click="playAudio('b')">/b/</div>
+        <div class="bg-gray-400" @click="playAudio('d')">/d/</div>
+        <div class="bg-gray-400" @click="playAudio('g')">/ɡ/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400 flex justify-center items-center row-span-2">
+          摩擦音
+        </div>
+        <div class="bg-gray-400">清辅音</div>
+        <div class="bg-gray-400" @click="playAudio('f')">/f/</div>
+        <div class="bg-gray-400" @click="playAudio('s')">/s/</div>
+        <div class="bg-gray-400" @click="playAudio('ʃ')">/ʃ/</div>
+        <div class="bg-gray-400" @click="playAudio('θ')">/θ/</div>
+        <div class="bg-gray-400" @click="playAudio('h')">/h/</div>
+        <div class="bg-gray-400">浊辅音</div>
+        <div class="bg-gray-400" @click="playAudio('v')">/v/</div>
+        <div class="bg-gray-400" @click="playAudio('z')">/z/</div>
+        <div class="bg-gray-400" @click="playAudio('ʒ')">/ʒ/</div>
+        <div class="bg-gray-400" @click="playAudio('ð')">/ð/</div>
+        <div class="bg-gray-400" @click="playAudio('r')">/r/</div>
+        <div class="bg-gray-400 flex justify-center items-center row-span-2">
+          破擦音
+        </div>
+        <div class="bg-gray-400">清辅音</div>
+        <div class="bg-gray-400" @click="playAudio('tʃ')">/tʃ/</div>
+        <div class="bg-gray-400" @click="playAudio('tr')">/tr/</div>
+        <div class="bg-gray-400" @click="playAudio('ts')">/ts/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400">浊辅音</div>
+        <div class="bg-gray-400" @click="playAudio('dʒ')">/dʒ/</div>
+        <div class="bg-gray-400" @click="playAudio('dr')">/dr/</div>
+        <div class="bg-gray-400" @click="playAudio('dz')">/dz/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400">鼻音</div>
+        <div class="bg-gray-400">（浊辅音）</div>
+        <div class="bg-gray-400" @click="playAudio('m')">/m/</div>
+        <div class="bg-gray-400" @click="playAudio('n')">/n/</div>
+        <div class="bg-gray-400" @click="playAudio('ŋ')">/ŋ/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400">舌则音</div>
+        <div class="bg-gray-400">（浊辅音）</div>
+        <div class="bg-gray-400" @click="playAudio('l')">/l/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400">半元音</div>
+        <div class="bg-gray-400">（浊辅音）</div>
+        <div class="bg-gray-400" @click="playAudio('j')">/j/</div>
+        <div class="bg-gray-400" @click="playAudio('w')">/w/</div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+        <div class="bg-gray-400"></div>
+      </div>
+
+      <footer class="text-white">
+        <ul class="list-disc">
+          <li>
+            音频来源：https://www.bilibili.com/video/BV1Y4411M7Ac?from=search&seid=12717483388753801542
+          </li>
+        </ul>
+      </footer>
     </div>
     <div></div>
   </div>
 </template>
 
 <script>
-import _ from "lodash";
+import { dic } from "./phonetic-symbol-number-dic";
 
 export default {
   name: "App",
   methods: {
-    // playAudio(num) {
-    //   console.log(num);
-    //   var audio = new Audio(require(`./assets/mp3s/02.mp3`)); // path to file
-    //   audio.play();
-    //   // this.d()
-    //   _.debounce(() => {}, 550);
-    // },
-    playAudio: _.debounce(() => {
-      if (audio) {
-        audio.ended()
-      }
-      var audio = new Audio(require(`./assets/mp3s/02.mp3`)); // path to file
+    playAudio (ps) {
+      var audio = new Audio(require(`./assets/mp3s/${dic[ps]}.mp3`)); // path to file
       audio.play();
-    }, 200),
-    d() {},
+    },
   },
 };
 </script>
